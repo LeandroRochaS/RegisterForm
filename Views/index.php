199e927bed4a76077237ee961c342216dc1a15d6
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-09 13:52:13 -0300

    fix: add folders for images

--- Views/index.php
+++ Views/index.php
@@ -20,7 +20,7 @@
       <div class="container py-5 h-100">
         <div class="row d-flex align-items-center justify-content-center h-100">
           <div class="col-md-8 col-lg-7 col-xl-6">
-            <img src="../Login-rafiki.svg" class="img-fluid" alt="Phone image">
+            <img src="../assets/img/Login-rafiki.svg" class="img-fluid" alt="Phone image">
           </div>
 
           <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
